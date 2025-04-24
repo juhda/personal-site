ba429bf299408fdcedee422acd0555b7fca88bf9
Dávid Juhász <juhasz.david@outlook.com>
2025-04-24 17:35:57 +0000

    Refactor site configuration and update components to utilize new imports

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+// Below should not change normally. If any does, check also in the SEO component.
+export const author = "Dávid Juhász";
+export const localMeImage = "/images/me.jpg";
+export const jobTitle = "Compiler & Systems Engineer";
+export const socialLinks = {
+  email: "mailto:hello@davidjuhasz.dev",
+  linkedin: "https://linkedin.com/in/juhaszdavid",
+  github: "https://github.com/juhda",
+};
+export const worksForOrgName = null;
+export const siteName = `${author} - ${jobTitle}`;
+export const defaultOGImage = "/og/default.png";
