f49f47f45eda863f191b8c86a8b2858375408d83
Dávid Juhász <juhasz.david@outlook.com>
2025-04-24 18:02:07 +0000

    Update index page

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 // Below should not change normally. If any does, check also in the SEO component.
 export const author = "Dávid Juhász";
-export const localMeImage = "/images/me.jpg";
+export const localMeImage = "/images/me.jpg"; // Relative to public folder. If you change the image, change src/assets/me.jpg as well!
 export const jobTitle = "Compiler & Systems Engineer";
 export const socialLinks = {
   email: "mailto:hello@davidjuhasz.dev",
