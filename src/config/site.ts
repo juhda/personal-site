b53520f48a7b8af4ac152603df6ec1ce70aca894
Dávid Juhász <juhasz.david@outlook.com>
2025-04-26 15:55:21 +0000

    Refactor job title usage across pages for consistency

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 // Below should not change normally. If any does, check also in the SEO component.
 export const author = "Dávid Juhász";
 export const localMeImage = "/images/me.jpg"; // Relative to public folder. If you change the image, change src/assets/me.jpg as well!
-export const jobTitle = "Compiler & Systems Engineer";
+export const jobTitle = "Compiler & Systems Engineer"; // This and cleanJobTitle are used at multiple places, so be careful when changing them.
 export const socialLinks = {
   email: "mailto:hello@davidjuhasz.dev",
   linkedin: "https://linkedin.com/in/juhaszdavid",
@@ -10,3 +10,6 @@ export const socialLinks = {
 export const worksForOrgName = null;
 export const siteName = `${author} - ${jobTitle}`;
 export const defaultOGImage = "/og/default.png";
+
+// Absolutely no need to change anything below this line.
+export const cleanJobTitle = jobTitle.replace(/&/g, 'and').toLowerCase();
