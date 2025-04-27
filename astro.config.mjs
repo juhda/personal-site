00354b4e78074dd8232e1cf613b45f1ff1424d2f
Dávid Juhász <juhasz.david@outlook.com>
2025-04-27 19:16:39 +0000

    Try generate redirects from Astro with adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davidjuhasz.dev',
   integrations: [tailwind(), sitemap()],
-  //redirects: {
-  //  "/resume": "/resume/DavidJuhasz_Resume_2025.pdf",
-  //}
+
+  // Enable configured redirects only when your host supports dynamic routing.
+  // For static hosts, use public/_redirects instead, and keep this option disabled.
+  // NOTE: Keep the list in sync with public/_redirects!
+  adapter: netlify(),
+  redirects: {
+    "/resume": {
+      status: 302,
+      destination: "/resume/DavidJuhasz_Resume_2025.pdf",
+    },
+  },
 });
