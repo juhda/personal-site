cef279432b961c29d6850177fbbf3437bd2dbdd8
Dávid Juhász <juhasz.david@outlook.com>
2025-04-27 21:28:59 +0200

    Fix resume redirection when deployed to Netlify
    Use Astro configured redirects with Netlify adapter for proper server-side redirection
    that enables directly fetching the actual resume document by the redirect link.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davidjuhasz.dev',
   integrations: [tailwind(), sitemap()],
+
+  // Adapter needed to handle redirects when deploying to Netlify
+  adapter: netlify(),
+  redirects: {
+    "/resume": {
+      status: 302,
+      destination: "/resume/DavidJuhasz_Resume_2025.pdf",
+    },
+  },
 });
