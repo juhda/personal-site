18e865cc77c919c033abda9a8be27f29de4f105c
Dávid Juhász <juhasz.david@outlook.com>
2025-04-29 18:03:58 +0200

    Add display name to mailto link
    Get a nicer look in browsers and mail clients that support it

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ export const author = "Dávid Juhász";
 export const localMeImage = "/images/me.jpg"; // Relative to public folder. If you change the image, change src/assets/me.jpg as well!
 export const jobTitle = "Compiler & Systems Engineer"; // This and cleanJobTitle are used at multiple places, so be careful when changing them.
 export const socialLinks = {
-  email: "mailto:hello@davidjuhasz.dev",
+  email: "mailto:Dávid%20Juhász%20<hello@davidjuhasz.dev>",
   linkedin: "https://linkedin.com/in/juhaszdavid",
   github: "https://github.com/juhda",
 };
